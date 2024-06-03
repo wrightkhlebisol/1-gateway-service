b70ea3a3df74aabf59813af41d3c31ce89353b7a
wrightkhlebisol <calebogundiya@gmail.com>
2024-06-03 14:31:42 +0100

    feat: add health routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
-import { healthRoutes } from '@gateway/routes/health';
 import { Application } from 'express';
+import { authRoutes } from '@gateway/routes/auth';
+import { healthRoutes } from '@gateway/routes/health';
+
+const BASE_PATH = '/api/gateway/v1';
 
 export const appRoutes = (app: Application) => {
-  app.use(healthRoutes.routes());
-
+  app.use('', healthRoutes.routes());
+  app.use(BASE_PATH, authRoutes.routes());
 };

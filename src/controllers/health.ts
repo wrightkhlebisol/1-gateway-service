18ccc7a82450af35b8238ef52aa57765869273f9
wrightkhlebisol <calebogundiya@gmail.com>
2024-06-03 14:36:33 +0100

    feat: update gateway health message

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -3,6 +3,6 @@ import { StatusCodes } from 'http-status-codes';
 
 export class Health {
   public static health(_req: Request, res: Response): void {
-    res.status(StatusCodes.OK).send('API Gateway service is health and OK.');
+    res.status(StatusCodes.OK).send('API Gateway service is healthy and OK.');
   }
 }
